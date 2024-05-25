807dae69d66810a7a6c90839b237fa68e57f5762
seiya <20365512+seiyab@users.noreply.github.com>
2024-05-25 21:56:48 +0900

    add bfinal into block

--- src/deflate/decode/mod.rs
+++ src/deflate/decode/mod.rs
@@ -8,37 +8,49 @@ struct Deflate {
 impl Deflate {
     pub fn decode<I: iter::Iterator<Item = u8>>(deflate: &mut I) -> Result<Self> {
         let mut blocks = Vec::new();
-        let b = Block::decode(deflate)?;
-        blocks.push(b);
+        loop {
+            let b = Block::decode(deflate)?;
+            let bfinal = b.bfinal;
+            blocks.push(b);
+            if bfinal {
+                break;
+            }
+        }
         return Ok(Self { blocks: blocks });
     }
 }
 
-enum Block {
-    Raw(RawBlock),
+struct Block {
+    bfinal: bool,
+    body: BlockBody,
 }
 
 impl Block {
     fn decode<I: iter::Iterator<Item = u8>>(it: &mut I) -> Result<Self> {
         let mut p = it.peekable();
-        let head = p.next().ok_or(Error::new("Unexpected EOF"))?;
+        let head = p.peek().ok_or(Error::new("Unexpected EOF"))?;
         let bfinal = head & 0x01;
         let btype = (head >> 1) & 0x03;
 
-        if bfinal != 1 {
-            return Err(Error::new("Unsupported bfinal"));
-        }
-
         match btype {
             0 => {
+                _ = p.next();
                 let raw = RawBlock::decode(&mut p)?;
-                return Ok(Block::Raw(raw));
+                let body = BlockBody::Raw(raw);
+                return Ok(Self {
+                    bfinal: bfinal == 1,
+                    body: body,
+                });
             }
             _ => {
                 return Err(Error::new("Unsupported block type"));
             }
         }
     }
+}
+
+enum BlockBody {
+    Raw(RawBlock),
 }
 
 struct RawBlock {
@@ -88,7 +100,7 @@ mod tests {
         let deflate: Vec<u8> = vec![0x01, 0x00, 0x00, 0xff, 0xff];
         let d = Deflate::decode(&mut deflate.iter().copied()).unwrap();
         assert_eq!(d.blocks.len(), 1);
-        if let Block::Raw(r) = &d.blocks[0] {
+        if let BlockBody::Raw(r) = &d.blocks[0].body {
             assert_eq!(r.data.len(), 0);
         } else {
             panic!("Unexpected block type");
